b3451432427f5a333a3e4ae6db61b1effd554330
aurelwidyanti <aurelwyt@gmail.com>
2025-01-09 14:30:12 +0700

    feat: Add CourseCategoryResource for transforming course category data into JSON format

---
+++ app/Http/Resources/CourseCategoryResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CourseCategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'created_by' => $this->created_by,
+            'user' => new UserResource($this->whenLoaded('user')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
