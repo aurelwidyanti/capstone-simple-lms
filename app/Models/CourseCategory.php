c1bdb819a1b8d91021ee0d238879576a6ff3ce4f
aurelwidyanti <aurelwyt@gmail.com>
2025-01-09 14:29:42 +0700

    feat: Add CourseCategory model and migration for managing course categories

---
+++ app/Models/CourseCategory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class CourseCategory extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+        'created_by',
+    ];
+
+    public function setSlugAttribute($value)
+    {
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function courses()
+    {
+        return $this->hasMany(Course::class, 'category_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
